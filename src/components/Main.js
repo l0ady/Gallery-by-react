1e9e9de50f854cf7eb815a797eaf59dacc6594b7
l0ady <546451014@qq.com>
2017-09-28 07:55:31 +0800

    update view style...

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,6 +7,19 @@ let yeomanImage = require('../images/yeoman.png');
 
 var imageDatas = require('../data/imageDatas.json');
 
+var ImgFigure = React.createClass({
+  render:function(){
+    return (
+        <figure className="img-figure">
+          <img src={this.props.data.url} alt={this.props.data.title}/>
+          <figcaption>
+            <h2 className="img-title">{this.props.data.title}</h2>
+          </figcaption>
+        </figure>
+      );
+  }
+});
+
 imageDatas = (function(imageList){
   for (var i = imageList.length - 1; i >= 0; i--) {
     var singleImage = imageList[i];
@@ -18,10 +31,16 @@ imageDatas = (function(imageList){
 
 class AppComponent extends React.Component {
   render() {
+    var controllerUnits = [], imgFigures = [];
+    let i = 0;
+    imageDatas.forEach(function(value){
+      imgFigures.push(<ImgFigure key={i++} data={value}/>);
+    })
+
     return (
       <section className="stage">
-        <section className="img-sec"></section>
-        <nav className="controller-nav"></nav>
+        <section className="img-sec">{imgFigures}</section>
+        <nav className="controller-nav">{controllerUnits}</nav>
       </section>
     );
   }
